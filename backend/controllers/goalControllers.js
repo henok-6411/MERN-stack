8d92a1438ddc93d3064ac4d1ccd7e94146034945
henok <henok@henoks-MacBook-Pro.local>
2023-02-07 13:15:40 -0800

    mongoDB had connected and schema work has been done

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,34 +1,58 @@
 const asyncHandler = require('express-async-handler');
+const Goal = require('../model/goalSchema');
 
 //@description Get all goals
 //@route GET '/api/goals'
 //@privacy Private
 
 const getGoal = asyncHandler(async (req, res) => {
-  res.send({ message: 'get goal' });
+  const goals = await  Goal.find()
+  if (!goals) {
+    res.status(400)
+    throw new Error('wrong request');
+  }
+  res.send(goals);
 });
 
 //@description Set all goals
 //@route POST '/api/goals'
 //@privacy Private
 const setGoal = asyncHandler(async (req, res) => {
-  if (!req.body.test) {
-    res.status(400);
-    throw new Error('insert valid input');
+  const goals = await Goal.create({
+    text:req.body.text
+  });
+  if (!goals) {
+    res.status(400)
+    throw new Error('Wrong reqeust')
   }
-  res.send({ message: 'set goal' });
+  res.send(goals);
 });
 //@description update goals
 //@route PUT '/api/goals/:id'
 //@privacy Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.send({ message: `set goal by ${req.params.id}` });
+  const goals = await Goal.findById(req.params.id);
+
+  if (!goals) {
+    res.status(400);
+    throw new Error('wrong request');
+  }
+  const updatedGoals = await Goal.findByIdAndUpdate( req.params.id, req.body , { new:true});
+  res.send(updatedGoals);
 });
 //@description delete goals
 //@route DELETE '/api/goals/:id'
 //@privacy Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.send({ message: `delete goal by ${req.params.id}` });
+  const goals = await Goal.findById(req.params.id);
+
+  if (!goals) {
+    res.status(400);
+    throw new Error('request not found')
+  }
+  const deletedGoal = await Goal.findByIdAndDelete(goals)
+  res.send(deletedGoal)
+
 });
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const router = require('./routers/goalRouts');
 const express = require('express');
 const errorHandler = require('./middleware/errorMiddleware');
-const PORT = process.env.PORT || 5000;
+const connectDb = require('./config/db');
+const PORT = process.env.PORT || 8000;
+connectDb()
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use('/api/goals', router);
 app.use(errorHandler);
